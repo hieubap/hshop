3abeedde6b86a7ef7d20550bb75081156a4000c3
ngohi <ngohieu1811@gmail.com>
2021-02-22 21:39:32 +0700

    sql is native

--- src/main/java/com/hshop/dao/repository/ProductRepository.java
+++ src/main/java/com/hshop/dao/repository/ProductRepository.java
@@ -15,15 +15,15 @@ public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
           + " and ( LOWER(f.title) like concat('%',LOWER(:#{#dto.title}),'%') or LOWER(:#{#dto.title}) is null ) "
           + " and ( LOWER(f.name) like concat('%',LOWER(:#{#dto.name}),'%') or LOWER(:#{#dto.name}) is null ) "
           + " and ( f.star = :#{#dto.star} or :#{#dto.star} is null ) "
-          + " and ( f.numberComment = :#{#dto.numberComment} or :#{#dto.numberComment} is null ) "
-          + " and ( f.numberSell = :#{#dto.numberSell} or :#{#dto.numberSell} is null ) "
+          + " and ( f.number_comment = :#{#dto.numberComment} or :#{#dto.numberComment} is null ) "
+          + " and ( f.number_sell = :#{#dto.numberSell} or :#{#dto.numberSell} is null ) "
           + " and ( f.number = :#{#dto.number} or :#{#dto.number} is null ) "
-          + " and ( f.oldPrice = :#{#dto.oldPrice} or :#{#dto.oldPrice} is null ) "
-          + " and ( f.newPrice = :#{#dto.newPrice} or :#{#dto.newPrice} is null ) "
+          + " and ( f.old_price = :#{#dto.oldPrice} or :#{#dto.oldPrice} is null ) "
+          + " and ( f.new_price = :#{#dto.newPrice} or :#{#dto.newPrice} is null ) "
           + " and ( LOWER(f.type) like concat('%',LOWER(:#{#dto.type}),'%') or LOWER(:#{#dto.type}) is null ) "
           + " and ( LOWER(f.color) like concat('%',LOWER(:#{#dto.color}),'%') or LOWER(:#{#dto.color}) is null ) "
-          + " and ( LOWER(f.tradeMark) like concat('%',LOWER(:#{#dto.tradeMark}),'%') or LOWER(:#{#dto.tradeMark}) is null ) "
-          + " and ( f.manufactureCountry = :#{#dto.manufactureCountry} or :#{#dto.manufactureCountry} is null ) "
+          + " and ( LOWER(f.trade_mark) like concat('%',LOWER(:#{#dto.tradeMark}),'%') or LOWER(:#{#dto.tradeMark}) is null ) "
+          + " and ( f.manufacture_country = :#{#dto.manufactureCountry} or :#{#dto.manufactureCountry} is null ) "
       , nativeQuery = true
   )
   List<ProductEntity> search(ProductDTO dto);
